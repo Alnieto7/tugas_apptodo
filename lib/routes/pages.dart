1e4642ee640e1967f05c3853749e46f4af8c3791
andrean <andreanawan634@gmail.com>
2025-09-09 19:26:35 +0700

    (AA) pages and routes

--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -1,3 +1,18 @@
-class AppPages{
+import 'package:get/get.dart';
+import 'package:tugas_apptodo/pages/DashboardPage.dart';
+import 'package:tugas_apptodo/pages/HomePage.dart';
+import 'package:tugas_apptodo/pages/LoginPage.dart';
+import 'package:tugas_apptodo/pages/TodoPage.dart';
+import 'package:tugas_apptodo/routes/routes.dart';
+import '../bindings/auth_binding.dart';
+import '../bindings/home_binding.dart';
 
+
+class AppPages {
+static final pages = [
+GetPage(name: Routes.LOGIN, page: () => LoginPage(), binding: AuthBinding()),
+GetPage(name: Routes.DASHBOARD, page: () => DashboardPage(), binding: HomeBinding()),
+GetPage(name: Routes.ADD_TODO, page: () => AddTodoPage(), binding: HomeBinding()),
+GetPage(name: Routes.HOME, page: () => HomePage(), binding: HomeBinding()),
+];
 }
