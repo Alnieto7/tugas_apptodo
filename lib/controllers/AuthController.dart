bf522bed2a98fb5653dc0540b81d9c6eb9d2853e
Alnieto7 <05532@smkrus.sch.id>
2025-09-10 09:42:12 +0700

    [AO] perbaikan login

--- lib/controllers/AuthController.dart
+++ lib/controllers/AuthController.dart
@@ -1,14 +1,34 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:tugas_apptodo/pages/DashboardPage.dart';
+import 'package:tugas_apptodo/pages/LoginPage.dart';
+
 
 class AuthController extends GetxController {
-  var Loginpage = false.obs;
+  final usernameC = TextEditingController();
+  final passwordC = TextEditingController();
 
-  void login(String username, String password) {
-    if (username == "admin" && password == "1234") {
-      Loginpage.value = true;
-      Get.offAllNamed('/dashboard');
+  var isLoggedIn = false.obs;
+
+  void login() {
+    if (usernameC.text == "admin" && passwordC.text == "1234") {
+      isLoggedIn.value = true;
+      Get.offAll(() => const DashboardPage());
+      Get.snackbar("Login Berhasil", "Selamat datang ${usernameC.text}",
+          snackPosition: SnackPosition.BOTTOM);
     } else {
-      Get.snackbar("Login Gagal", "Username atau password salah");
+      Get.offAll(() => LoginPage());
+      Get.snackbar("Login Gagal", "Username atau password salah",
+          backgroundColor: Colors.red, colorText: Colors.white,
+          );
+
     }
+  }
+
+  @override
+  void onClose() {
+    usernameC.dispose();
+    passwordC.dispose();
+    super.onClose();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_core/get_core.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:tugas_apptodo/color/theme.dart';
+import 'package:tugas_apptodo/pages/DashboardPage.dart';
 import 'package:tugas_apptodo/pages/LoginPage.dart';
 import 'bindings/auth_binding.dart';
 import 'bindings/home_binding.dart';
@@ -21,6 +23,18 @@ class MyApp extends StatelessWidget {
       theme: AppTheme.lightTheme,
       home:  LoginPage(),
       initialBinding: AuthBinding(),
+      getPages: [
+        GetPage(
+          name: '/dashboard',
+          page: () => DashboardPage(),
+          binding: HomeBinding(),
+        ),
+        GetPage(
+          name: '/login',
+          page: () => LoginPage(),
+          binding: AuthBinding(),
+        ),
+      ],
     );
   }
 }
